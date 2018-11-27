30e70ad0e8859d893a1ed4042fa5799fe6263707
Sakayama <44540001+Sakayama@users.noreply.github.com>
2018-11-27 13:37:31 +0700

    refactor types

--- songs.hs
+++ songs.hs
@@ -1,43 +1,46 @@
-main = putStrLn $ show $ shortestSong songs
+main = putStrLn $ show $ totalTime songs
 
 -- returns a list of titles
 
-getTitles :: [(String, b, c)] -> [String]
+getTitles :: [Song] -> [String]
 getTitles = map (\(title, _, _) -> title)
 
-getTitles' :: [(String, b, c)] -> [String]
+getTitles' :: [Song] -> [String]
 getTitles' xs = [title | (title, _, _) <- xs]
 
-getTitles'' :: [(String, b, c)] -> [String]
+getTitles'' :: [Song] -> [String]
 getTitles'' [] = []
 getTitles'' ((title, _, _):xs) = title : getTitles'' xs
 
 -- returns total time of the playlist
 
-totalTime :: [(a, b, Float)] -> Float
+totalTime :: [Song] -> Float
 totalTime xs = sum [time | ( _, _, time) <- xs]
 
-totalTime' :: [(a, b, Float)] -> Float
-totalTime' = foldl (\acc (a, b, time) -> acc + time::Float) 0
+totalTime' :: [Song] -> Float
+totalTime' = foldl (\acc (a, b, time) -> acc + time) 0
 
-totalTime'' :: [(a, b, Float)] -> Float
+totalTime'' :: [Song] -> Float
 totalTime'' [] = 0
 totalTime'' ((a, b, time):tail) = time + totalTime'' tail
 
 -- returns the longest song
-longestSong :: (Ord c) => [(a, b, c)] -> (a, b, c)
+longestSong :: [Song] -> Song
 longestSong [x] = x
 longestSong (x@(_, _, timeX):xs) = if timeX > timeY then x else y
   where y@( _, _, timeY) = longestSong xs
 
 -- returns the shortest song
-shortestSong :: (Ord c) => [(a, b, c)] -> (a, b, c)
+shortestSong :: [Song] -> Song
 shortestSong [x] = x
 shortestSong (x@(_, _, timeX):xs) = if timeX < timeY then x else y
   where y@( _, _, timeY) = shortestSong xs
 
+-- types
+type Song = (String, Float, Float)
+
 -- data sourses
-songs :: [(String, Float, Float)]
+songs :: [Song]
 songs = [
     ("Song for Siren", 15.0, 4.35),
     ("Mamma Mia", 1.0, 4.5),
@@ -49,6 +52,7 @@ songs = [
     ("Block", 5.0, 4.35)
         ]
 
+songs2 :: [Song] 
 songs2 = [
     ("Song for Siren", 15.0, 4.35),
     ("Lalala", 22.0, 4.0),
