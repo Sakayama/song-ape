656b05c3bc5d70c9934c3c7f49ddf39273d58ca9
Sakayama <44540001+Sakayama@users.noreply.github.com>
2018-11-27 13:25:29 +0700

    add longest and shortest song

--- songs.hs
+++ songs.hs
@@ -1,25 +1,43 @@
-main = putStrLn $ show $ totalTime' songs
+main = putStrLn $ show $ shortestSong songs
 
+-- returns a list of titles
+
+getTitles :: [(String, b, c)] -> [String]
 getTitles = map (\(title, _, _) -> title)
 
+getTitles' :: [(String, b, c)] -> [String]
 getTitles' xs = [title | (title, _, _) <- xs]
 
+getTitles'' :: [(String, b, c)] -> [String]
 getTitles'' [] = []
 getTitles'' ((title, _, _):xs) = title : getTitles'' xs
+
+-- returns total time of the playlist
 
 totalTime :: [(a, b, Float)] -> Float
 totalTime xs = sum [time | ( _, _, time) <- xs]
 
+totalTime' :: [(a, b, Float)] -> Float
 totalTime' = foldl (\acc (a, b, time) -> acc + time::Float) 0
-
-totalTimeErunda = foldl (flip (\(_, _, t) -> (+) (t::Float))) 0
 
 totalTime'' :: [(a, b, Float)] -> Float
 totalTime'' [] = 0
 totalTime'' ((a, b, time):tail) = time + totalTime'' tail
 
+-- returns the longest song
+longestSong :: (Ord c) => [(a, b, c)] -> (a, b, c)
+longestSong [x] = x
+longestSong (x@(_, _, timeX):xs) = if timeX > timeY then x else y
+  where y@( _, _, timeY) = longestSong xs
 
+-- returns the shortest song
+shortestSong :: (Ord c) => [(a, b, c)] -> (a, b, c)
+shortestSong [x] = x
+shortestSong (x@(_, _, timeX):xs) = if timeX < timeY then x else y
+  where y@( _, _, timeY) = shortestSong xs
 
+-- data sourses
+songs :: [(String, Float, Float)]
 songs = [
     ("Song for Siren", 15.0, 4.35),
     ("Mamma Mia", 1.0, 4.5),
