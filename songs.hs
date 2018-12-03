bf06795a0f77b8af9d53cd2d0b2a63cf5d9f25e2
Sakayama <44540001+Sakayama@users.noreply.github.com>
2018-12-03 15:55:27 +0700

    add first, last, sorting, set and replace

--- songs.hs
+++ songs.hs
@@ -1,7 +1,31 @@
-main = putStrLn $ show $ totalTime songs
+main = putStrLn $ show $ totalTime''' songs
+
+-- returns the first song
+firstSong :: [Song] -> Song
+firstSong (x:xs) = x
+
+firstSong' :: [a] -> a
+firstSong' = head
+
+-- returns the last song
+lastSong :: [Song] -> Song
+lastSong [x] = x
+lastSong (_:xs) = lastSong xs
+
+lastSong' :: [a] -> a
+lastSong' = last
+
+-- takes new title and a list. Returns a new list with a new title1
+setFirstTitle :: String -> [Song] -> [Song]
+setFirstTitle title ((_, price, duration):xs) = (title, price, duration) : xs
+
+-- takes previous title, a new title, then replaces all titles equal to previous
+replaceTitles :: String -> String -> [Song] -> [Song]
+replaceTitles targetTitle newTitle = map (\(previousTitle, price, duration) -> 
+  let title = if previousTitle == targetTitle then newTitle else previousTitle 
+  in (title, price, duration))
 
 -- returns a list of titles
-
 getTitles :: [Song] -> [String]
 getTitles = map (\(title, _, _) -> title)
 
@@ -18,11 +42,11 @@ totalTime :: [Song] -> Float
 totalTime xs = sum [time | ( _, _, time) <- xs]
 
 totalTime' :: [Song] -> Float
-totalTime' = foldl (\acc (a, b, time) -> acc + time) 0
+totalTime' = foldl (\acc (_, _, time) -> acc + time) 0
 
 totalTime'' :: [Song] -> Float
 totalTime'' [] = 0
-totalTime'' ((a, b, time):tail) = time + totalTime'' tail
+totalTime'' ((_, _, time):tail) = time + totalTime'' tail
 
 -- returns the longest song
 longestSong :: [Song] -> Song
@@ -35,6 +59,37 @@ shortestSong :: [Song] -> Song
 shortestSong [x] = x
 shortestSong (x@(_, _, timeX):xs) = if timeX < timeY then x else y
   where y@( _, _, timeY) = shortestSong xs
+
+--   returns the most pricey song
+priceySong :: [Song] -> Song
+priceySong [x] = x
+priceySong (x@(_, priceX, _):xs) = if priceX > priceY then x else y
+  where y@(_, priceY, _) = priceySong xs
+
+-- returns the least expensive song
+leastExpensive :: [Song] -> Song
+leastExpensive [x] = x
+leastExpensive (x@(_, priceX, _):xs) = if priceX < priceY then x else y
+  where y@(_, priceY, _) = leastExpensive xs
+
+smallestPrice :: [Song] -> Float
+smallestPrice xs = price
+  where (_, price, _) = leastExpensive xs
+
+--takes a list of songs, returns a list of prices
+getPrices :: [Song] -> [Float]
+getPrices = map (\(title, price, duration) -> price)
+
+-- returns the same list of songs sorted by price
+sortByPrice :: [Song] -> [Song]
+sortByPrice [] = []
+sortByPrice (x:xs) = 
+  let lte (_, priceX, _) (_, priceY, _) = priceX <= priceY
+      gt (_, priceX, _) (_, priceY, _) = priceX > priceY    
+      smallerSorted = sortByPrice [a | a <- xs, lte a x]
+      biggerSorted = sortByPrice [a | a <- xs, gt a x]
+   in smallerSorted ++ [x] ++ biggerSorted
+
 
 -- types
 type Song = (String, Float, Float)
